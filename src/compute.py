aa05b136b864a0238fc8c9f752ae1636cf50d506
FiocChuck <chasf@google.com>
2024-01-25 16:59:25 -0500

    debug

--- src/compute.py
+++ src/compute.py
@@ -4,62 +4,64 @@ from functions import *
 
 def run(**kwargs):
 
-    num_media = kwargs.get('num_media', {})
-    sms_sid = kwargs.get('sms_sid', {})
-    sms_from = kwargs.get('sms_from', {})
-    body = kwargs.get('body', {})
-    media_url = kwargs.get('media_url', {})
+    try:
+        num_media = kwargs.get('num_media', {})
+        sms_sid = kwargs.get('sms_sid', {})
+        sms_from = kwargs.get('sms_from', {})
+        body = kwargs.get('body', {})
+        media_url = kwargs.get('media_url', {})
 
-    # return flask forms as dict
-    # data = request.form
+        # return flask forms as dict
+        # data = request.form
 
-    #  sms or mms - zero indicates sms
-    flag = str(num_media)
+        #  sms or mms - zero indicates sms
+        flag = str(num_media)
 
-    #  gcs bucket name - images saved here
-    bucket_name = "twillio-images"
+        #  gcs bucket name - images saved here
+        bucket_name = "twillio-images"
 
-    # required to initialize vertex client
-    project = "cf-data-analytics"
-    loc = "us-central1"
+        # required to initialize vertex client
+        project = "cf-data-analytics"
+        loc = "us-central1"
 
-    # process multimedia messages (mms)
-    if flag != "0":
+        # process multimedia messages (mms)
+        if flag != "0":
 
-        # download image sms_from twillio
-        mms_process(media_url, sms_sid)
+            # download image sms_from twillio
+            mms_process(media_url, sms_sid)
 
-        #  set filename of images sent to gcs. Use unique identifier sms_from twillio
-        filename = sms_sid + ".png"
+            #  set filename of images sent to gcs. Use unique identifier sms_from twillio
+            filename = sms_sid + ".png"
 
-        # Upload image to gcs bucket
-        upload_blob(bucket_name, filename, filename)
+            # Upload image to gcs bucket
+            upload_blob(bucket_name, filename, filename)
 
-        # save metadata to firestore collection 1 (filename, masked identifier)
-        save_results_collection1(
-            sms_sid, number_mask(sms_from), filename)
+            # save metadata to firestore collection 1 (filename, masked identifier)
+            save_results_collection1(
+                sms_sid, number_mask(sms_from), filename)
 
-        # create results doc
-        save_results_collection2(sms_sid, filename)
+            # create results doc
+            save_results_collection2(sms_sid, filename)
 
-    # process text messages
-    else:
+        # process text messages
+        else:
 
-        #  retrieve last image URL uploaded by same user sms_from Firestore
-        time.sleep(45)
-        doc = return_image(number_mask(sms_from))
-        filename = doc["fileName"]
+            #  retrieve last image URL uploaded by same user sms_from Firestore
+            time.sleep(45)
+            doc = return_image(number_mask(sms_from))
+            filename = doc["fileName"]
 
-        # construct gsl using filename
-        path = "gs://" + bucket_name + "/" + filename  # uri
+            # construct gsl using filename
+            path = "gs://" + bucket_name + "/" + filename  # uri
 
-        # genrate response using gemini
-        genai_ouput = generate_text(
-            project, loc, path, body)
+            # genrate response using gemini
+            genai_ouput = generate_text(
+                project, loc, path, body)
 
-        print(genai_ouput)
+            print(genai_ouput)
 
-        update_collection2(doc["fileName"].split(
-            ".")[0], body, genai_ouput)
+            update_collection2(doc["fileName"].split(
+                ".")[0], body, genai_ouput)
 
-    return
+    except Exception as e:
+        print(e)
--- src/main.py
+++ src/main.py
@@ -9,36 +9,32 @@ import time
 @functions_framework.http
 def main(request):
 
-    try:
+    data = request.form
 
-        data = request.form
+    if request.method == "OPTIONS":
+        # Allows GET requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET",
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Max-Age": "3600",
+        }
 
-        if request.method == "OPTIONS":
-            # Allows GET requests from any origin with the Content-Type
-            # header and caches preflight response for an 3600s
-            headers = {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "GET",
-                "Access-Control-Allow-Headers": "Content-Type",
-                "Access-Control-Max-Age": "3600",
-            }
+        return ("", 204, headers)
 
-            return ("", 204, headers)
+    # Set CORS headers for the main request
+    headers = {"Access-Control-Allow-Origin": "*"}
 
-        # Set CORS headers for the main request
-        headers = {"Access-Control-Allow-Origin": "*"}
+    thread = threading.Thread(target=run, kwargs={
+        'body': data["Body"],
+        'num_media': data["NumMedia"],
+        'sms_sid': data["SmsSid"],
+        'sms_from': data["From"],
+        'media_url': data["MediaUrl0"]})
+    thread.start()
 
-        thread = threading.Thread(target=run, kwargs={
-            'body': data["Body"],
-            'num_media': data["NumMedia"],
-            'sms_sid': data["SmsSid"],
-            'sms_from': data["From"],
-            'media_url': data["MediaUrl0"]})
-        thread.start()
-
-        return ("done", 200, headers)
-    except Exception as e:
-        print(e)
+    return ("done", 200, headers)
 
     # try:
 
