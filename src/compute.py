f0b2d2c6ee110226072daba3f2637d0d454d314e
FiocChuck <chasf@google.com>
2024-01-25 17:23:12 -0500

    debug

--- src/compute.py
+++ src/compute.py
@@ -17,7 +17,7 @@ def run(**kwargs):
         #  sms or mms - zero indicates sms
         flag = str(num_media)
 
-        print("flag" + flag)
+        print("flag: " + flag)
 
         #  gcs bucket name - images saved here
         bucket_name = "twillio-images"
@@ -29,18 +29,26 @@ def run(**kwargs):
         # process multimedia messages (mms)
         if flag != "0":
 
+            print("processing mms")
+
             # download image sms_from twillio
             mms_process(media_url, sms_sid)
 
             #  set filename of images sent to gcs. Use unique identifier sms_from twillio
             filename = sms_sid + ".png"
 
+            print("processing file: " + filename)
+
             # Upload image to gcs bucket
             upload_blob(bucket_name, filename, filename)
+
+            print("upload to bucket complete")
 
             # save metadata to firestore collection 1 (filename, masked identifier)
             save_results_collection1(
                 sms_sid, number_mask(sms_from), filename)
+
+            print("Meta data saved to Firestore")
 
             # create results doc
             save_results_collection2(sms_sid, filename)
@@ -49,12 +57,21 @@ def run(**kwargs):
         else:
 
             #  retrieve last image URL uploaded by same user sms_from Firestore
+
+            print("processing sms")
+
             time.sleep(1)
             doc = return_image(number_mask(sms_from))
+
+            print("doc returned from Firestore")
+            print(doc)
+
             filename = doc["fileName"]
 
             # construct gsl using filename
             path = "gs://" + bucket_name + "/" + filename  # uri
+
+            print("file path: " + path)
 
             # genrate response using gemini
             genai_ouput = generate_text(
